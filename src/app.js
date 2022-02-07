47503ecedc944382926b02e05fce5d7266e48a1d
Timothy Puesey <79171607+tpuesey@users.noreply.github.com>
2022-02-07 16:57:28 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,13 +9,17 @@ const moviesRouter = require("./movies/movies.router")
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use("/movies", moviesRouter)
-app.use("/reviews", reviewsRouter)
-app.use("/theaters", theatersRouter)
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+app.use("/", directory);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
+
+
+
 module.exports = app;
